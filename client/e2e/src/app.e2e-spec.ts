9c4d2154265c6386c2f8c3e260344a2a12cb6855
JoshuaHamann <haman127@morris.umn.edu>
2020-03-12 18:22:37 -0500

    Same commit

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -13,7 +13,7 @@ describe('App', () => {
 
   it('Should have the correct title', () => {
     page.navigateTo();
-    expect(page.getAppTitle()).toEqual('Virtual Post-It');
+    expect(page.getAppTitle()).toEqual('-It');
   });
 
   it('Should open the sidenav', () => {
